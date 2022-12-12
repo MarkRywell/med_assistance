203bdd97960754b89bd3173ff1a9546dfc7eefc8
MarkRywell <gopro855@gmail.com>
2022-12-12 22:44:26 +0800

    add patient form created

--- lib/views/patient_form.dart
+++ lib/views/patient_form.dart
@@ -12,78 +12,156 @@ class _PatientFormState extends State<PatientForm> {
 
   var formKey = GlobalKey<FormState>();
 
+  DateTime date = DateTime.now();
+
   TextEditingController nameController = TextEditingController();
   TextEditingController ageController = TextEditingController();
-  TextEditingController dateController = TextEditingController();
+
   TextEditingController diseaseController = TextEditingController();
   TextEditingController medicinesController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Add Patient"),
-      ),
-      body: SingleChildScrollView(
-        child: Form(
-            key: formKey,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: nameController,
-                    keyboardType: TextInputType.text,
-                    decoration: const InputDecoration(
-                      labelText: "Name",
-                      border: OutlineInputBorder()
-                    ),
-                    validator: (value) {
-                      return value == null || value.isEmpty ? "Enter patient's name" : null;
-                    },
-                  ),
-                  TextFormField(
-                    controller: ageController,
-                    keyboardType: TextInputType.text,
-                    decoration: const InputDecoration(
-                        labelText: "Name",
-                        border: OutlineInputBorder()
-                    ),
-                    validator: (value) {
-                      return value == null || value.isEmpty ? "Enter patient's age" : null;
-                    },
-                  ),
-
-                  //DATE PICKER HERE
-
-                  TextFormField(
-                    controller: diseaseController,
-                    keyboardType: TextInputType.text,
-                    decoration: const InputDecoration(
-                        labelText: "Disease Details",
-                        border: OutlineInputBorder()
-                    ),
-                    validator: (value) {
-                      return value == null || value.isEmpty ? "Enter patient's disease details" : null;
-                    },
-                  ),
-                  TextFormField(
-                    controller: medicinesController,
-                    keyboardType: TextInputType.text,
-                    decoration: const InputDecoration(
-                        labelText: "Medicines",
-                        border: OutlineInputBorder()
-                    ),
-                    validator: (value) {
-                      return value == null || value.isEmpty ? "Enter patient's medicine to administer" : null;
-                    },
-                  ),
-
-                ],
-              ),
-            )
+        appBar: AppBar(
+          title: const Text("Add Patient"),
         ),
-      )
+        body: SingleChildScrollView(
+          child: Form(
+              key: formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                      child: TextFormField(
+                        controller: nameController,
+                        keyboardType: TextInputType.text,
+                        decoration: const InputDecoration(
+                            labelText: "Name",
+                            border: OutlineInputBorder()
+                        ),
+                        validator: (value) {
+                          return value == null || value.isEmpty ? "Enter patient's name" : null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                      child: TextFormField(
+                        controller: ageController,
+                        keyboardType: TextInputType.text,
+                        decoration: const InputDecoration(
+                            labelText: "Name",
+                            border: OutlineInputBorder()
+                        ),
+                        validator: (value) {
+                          return value == null || value.isEmpty ? "Enter patient's age" : null;
+                        },
+                      ),
+                    ),
+
+                    //DATE PICKER HERE
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                      child: Container(
+                        width: size.width,
+                        padding: const EdgeInsets.symmetric(vertical: 5),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          border: Border.all(
+                              color: Colors.grey,
+                              width: 1
+                          ),
+                        ),
+                        child: Row(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.only(left: 20),
+                              child: Text('${date.month}/${date.day}/${date.year}',
+                                style: const TextStyle(
+                                    fontSize: 16
+                                ),),
+                            ),
+                            const VerticalDivider(width: 180),
+                            ElevatedButton(
+                                onPressed: () async {
+                                  DateTime? newDate = await showDatePicker(
+                                      context: context,
+                                      initialDate: date,
+                                      firstDate: DateTime(1900),
+                                      lastDate: DateTime(2100)
+                                  );
+                                  if(newDate == null){
+                                    return;
+                                  }
+                                  setState(() {
+                                    date = newDate;
+                                  });
+                                },
+                                child: const Icon(Icons.calendar_month))
+                          ],
+                        ),
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                      child: TextFormField(
+                        controller: diseaseController,
+                        keyboardType: TextInputType.text,
+                        maxLines: 5,
+                        decoration: const InputDecoration(
+                            labelText: "Disease Details",
+                            border: OutlineInputBorder()
+                        ),
+                        validator: (value) {
+                          return value == null || value.isEmpty ? "Enter patient's disease details" : null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                      child: TextFormField(
+                        controller: medicinesController,
+                        keyboardType: TextInputType.text,
+                        maxLines: 5,
+                        decoration: const InputDecoration(
+                            labelText: "Medicines",
+                            border: OutlineInputBorder()
+                        ),
+                        validator: (value) {
+                          return value == null || value.isEmpty ? "Enter patient's medicine to administer" : null;
+                        },
+                      ),
+                    ),
+                    const SizedBox(height:50),
+                    SizedBox(
+                      height: 40,
+                        width: size.width,
+                        child: ElevatedButton(
+                          onPressed: () {
+
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Text("Add Patient"),
+                              VerticalDivider(width: 10),
+                              Icon(Icons.person_add_alt_1_outlined)
+                            ],
+                          ),
+                        )
+                    )
+                  ],
+                ),
+              )
+          ),
+        )
     );
   }
 }
